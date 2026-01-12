a7e515418075728360d745c5409fe671447b9169
TusharTechie3 <shailesh.rawat@thoughts2binary.com>
2026-01-13 00:56:42 +0530

    initial commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <div className="mt-16 flex flex-col items-center gap-8 md:flex-row md:items-start md:justify-between md:gap-0 bg-gray-800 p-8 rounded-lg">
+      <div className="flex flex-col gap-4 items-center md:items-start">
+        <Link href="/" className="flex items-center">
+          <Image
+            src="/crop.svg"
+            alt="Thrifton Stores"
+            width={50}
+            height={30}
+            className="w-35 h-12 md:w-40 md:h-10"
+          />
+        </Link>
+        <p className="text-sm text-gray-400">© 2025 Trendlama.</p>
+        <p className="text-sm text-gray-400">All rights reserved.</p>
+      </div>
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm font-bold text-amber-50">Company</p>
+        <Link href="/">Homepage</Link>
+        <Link href="/">Contact</Link>
+        <Link href="/">Terms of Service</Link>
+        <Link href="/">Privacy Policy</Link>
+      </div>
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm font-bold text-amber-50">Resources</p>
+        <Link href="/">All Products</Link>
+        <Link href="/">New Arrivals</Link>
+        <Link href="/">Best Sellers</Link>
+        <Link href="/">Sale</Link>
+      </div>
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm font-bold text-amber-50">Socials</p>
+        <Link href="/">Facebook</Link>
+        <Link href="/">Instagram</Link>
+        <Link href="/">Twitter</Link>
+        <Link href="/">Affiliate Program</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+import SearchBar from "./SearchBar";
+import { Bell, Home, ShoppingCart } from "lucide-react";
+import ShoppingCartIcon from "./ShoppingCartIcon";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full flex items-center justify-between border-b border-gray-200 pb-4">
+      {/* LEFT */}
+      <Link href="/" className="flex items-center">
+        <Image
+          src="/crop.svg"
+          alt="Thrifton Stores"
+          width={30}
+          height={20}
+          className="w-28 h-9 md:w-40 md:h-10"
+        />
+        {/* <p className="hidden md:block text-md font-medium tracking-wider">
+          Thrifton Stores
+        </p> */}
+      </Link>
+      {/* RIGHT */}
+      <div className="flex items-center gap-6">
+        <SearchBar />
+        <Link href="/">
+          <Home className="w-4 h-4 text-gray-600" />
+        </Link>
+        <Bell className="w-4 h-4 text-gray-600" />
+        <ShoppingCartIcon />
+        <Link href="/login">Sign in</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
